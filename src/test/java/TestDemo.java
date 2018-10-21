d31ad1d13bf5d655ad6fe7e94b677a44aad7b39a
xiaoliu670 <xiaoliu670@aliyun.com>
2018-10-21 11:48:01 +0800

    第一次提交

---
+++ src/test/java/TestDemo.java
@@ -0,0 +1,15 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestDemo {
+    @Test
+    public void testcase1(){
+        Assert.assertTrue(true);
+        System.out.println("testcase1");
+    }
+    @Test
+    public void testcase2(){
+        Assert.assertTrue(true);
+        System.out.println("testcase1");
+    }
+}
